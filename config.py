e7055afeea830af8374e68d9bd0d42fe07ea81db
peaceville <uferepeace@gmail.com>
2019-06-18 09:03:18 +0100

    implemented followers, pagination and email support features

--- config.py
+++ config.py
@@ -14,4 +14,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['sagacitynow@yahoo.com']
+
+    ADMINS = ['noreply@flaskmicroblog.com']
+
+    POSTS_PER_PAGE = 25
